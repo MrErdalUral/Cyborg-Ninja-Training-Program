f8a091a4a6fb8287274ed57e3ad984c2df4c5abf
Erdal Ural <ahandaerdal@hotmail.com>
2016-12-11 01:03:35 +0300

    Katana

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ControlRotation();
+
+	}
+    protected void ControlRotation()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var lookingVector = mousePosition - transform.position;
+        var angle = Mathf.Atan2(lookingVector.y, lookingVector.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public bool SwitchDirection { get; set; }
+}
