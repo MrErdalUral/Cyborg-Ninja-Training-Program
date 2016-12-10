9be4258d8c3ef9eaa5c400f0ffc40d50680df8e1
Erdal Ural <ahandaerdal@hotmail.com>
2016-12-10 14:21:56 +0300

    Merge

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,5 +2,17 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed = 10f;
+    private Rigidbody2D _rigidbody;
 
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var movingVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        _rigidbody.velocity = movingVector * Speed;
+    }
 }
